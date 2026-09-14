b1279592185835fb019716f7b5f56b56003043b2
Matheus Diniz <matheusmoreiradinizz@gmail.com>
2026-09-14 20:55:58 -0300

    Delete services directory

--- services/tipo_culinaria/listar_tipos_culinaria_service.py
+++
@@ -1,9 +0,0 @@
-from backend.models.tipo_culinaria import TipoCulinaria
-
-
-class ListarTiposCulinariaService:
-    """Caso de uso: listar todos os tipos de culinária."""
-
-    @staticmethod
-    def execute():
-        return TipoCulinaria.listar_todos()
