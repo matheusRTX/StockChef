3d7f574d41f50c86193170e87a2183d5eb0735bc
Matheus Diniz <matheusmoreiradinizz@gmail.com>
2026-09-14 20:40:45 -0300

    Delete backend directory

--- backend/services/categoria/listar_categorias_service.py
+++
@@ -1,9 +0,0 @@
-from backend.models.categoria import Categoria
-
-
-class ListarCategoriasService:
-    """Caso de uso: listar todas as categorias."""
-
-    @staticmethod
-    def execute():
-        return Categoria.listar_todos()
